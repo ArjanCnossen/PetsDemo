d73a0b2b8589b5e872e5cde5da921cb2b03315e4
Arjan Cnossen <arjancnossenn@gmail.com>
2024-04-01 22:25:33 +0200

    Initial commit

---
+++ src/main/java/nl/miw/co13/arjan/petDemo/PetDemoApplication.java
@@ -0,0 +1,13 @@
+package nl.miw.co13.arjan.petDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miw/co13/arjan/petDemo/PetDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miw.co13.arjan.petDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
